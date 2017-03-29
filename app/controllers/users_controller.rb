fb8bf184bd9809f72eda1e7980caa2425fc0a0a2
TomWesterhout <westerhout_tom@hotmail.com>
2017-03-29 13:43:19 +0200

    Added edit and update functions and the display of error messages on the form page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,26 @@ class UsersController < ApplicationController
 	end
 
     def create
+    	#Instance variable used for the form_tag form and the pre-nested attributes of the classic html form.
     	#@user = User.new(username: params[:username], email: params[:email], password: params[:password])
     	@user = User.new(user_params)
     	if @user.save
     		redirect_to new_user_path
     	else
     		render :new
+    	end
+    end
+
+    def edit
+    	@user = User.find(params[:id])
+    end
+
+    def update
+    	@user = User.find(params[:id])
+    	if @user.update(user_params)
+    		redirect_to edit_user_path(@user)
+    	else
+    		render :edit
     	end
     end
 
